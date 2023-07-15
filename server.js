cb562ab073ce077e1a84689652e77102c542c7f0
Raval Vandan <vandanraval2002@gmail.com>
2023-07-15 15:10:54 +0530

    adding the serverfile and configfolder

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express =require("express");
-
+const serverCongig= require('./configs/server.config')
 const app=express();
 
-app.listen(3333,()=>{
-    console.log("Server Started")
+app.listen(serverCongig.PORT,()=>{
+    console.log(`SERVER STARTED ON PORT NUMBER ${serverCongig.PORT}`);
 
     //checking purpose
 })
